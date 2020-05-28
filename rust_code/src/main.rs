c5685f8922ee5d3689ded43c5ba5add0db7035a1
Victor Dominguez <vdominguez1993@gmail.com>
2020-05-28 21:59:39 +0200

    init changed to start and added parsing of the http request

--- rust_code/src/main.rs
+++ rust_code/src/main.rs
@@ -3,13 +3,10 @@ pub mod log;
 mod web_server;
 
 use log::*;
-use std::env;
 
 fn main() {
     log_init();
 
-    println!("{}", env::current_dir().unwrap().as_path().display());
-    web_server::init();
-
+    web_server::start();
     loop {}
 }
--- rust_code/src/web_server/mod.rs
+++ rust_code/src/web_server/mod.rs
@@ -8,14 +8,15 @@ use std::thread;
 use threadpool::ThreadPool;
 
 const THREADS_IN_POOL: usize = 4;
+const IP: &str = "0.0.0.0:7878";
 const WEB_ROOT: &str = "../web/";
 
-pub fn init() {
+pub fn start() {
     thread::spawn(|| server_thread());
 }
 
 fn server_thread() {
-    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    let listener = TcpListener::bind(IP).unwrap();
     let pool = ThreadPool::new(THREADS_IN_POOL);
 
     debug!("Server start");
@@ -39,6 +40,8 @@ fn handle_connection(mut stream: TcpStream) {
     debug!("Incoming data from {}", stream.peer_addr().unwrap());
     debug!("Received data:\n{}", trimmed_input);
 
+    let pair = parse_method_and_path(&buffer);
+    println!("method {} path {}", pair.0, pair.1);
     let get = b"GET / HTTP/1.1\r\n";
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
@@ -53,4 +56,29 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+fn parse_method_and_path(buffer: &[u8]) -> (String, String) {
+    let mut path = String::new();
+    let mut method = String::new();
+    let mut string_accumulated = String::new();
+    let mut element = 0;
+    for x in buffer {
+        match *x as char {
+            '\r' => break,
+            ' ' => {
+                element = element + 1;
+                match element {
+                    1 => method = string_accumulated.clone(),
+                    2 => path = string_accumulated.clone(),
+                    _ => {}
+                }
+                println!("Acumulated {}", string_accumulated);
+                string_accumulated.clear();
+            }
+            _ => string_accumulated.push(*x as char),
+        };
+    }
+
+    (method, path)
 }
