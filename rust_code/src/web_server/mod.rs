a96943afb6bf1782153557fd5624469dea1e4a3d
Victor Dominguez <vdominguez1993@gmail.com>
2020-05-29 19:50:49 +0200

    Server search automatically for paths and checks they are GET methods

--- rust_code/src/web_server/mod.rs
+++ rust_code/src/web_server/mod.rs
@@ -11,6 +11,8 @@ const THREADS_IN_POOL: usize = 4;
 const IP: &str = "0.0.0.0:7878";
 const WEB_ROOT: &str = "../web/";
 
+type ResponseHandler = fn(&str) -> String;
+
 pub fn start() {
     thread::spawn(|| server_thread());
 }
@@ -40,19 +42,22 @@ fn handle_connection(mut stream: TcpStream) {
     debug!("Incoming data from {}", stream.peer_addr().unwrap());
     debug!("Received data:\n{}", trimmed_input);
 
-    let pair = parse_method_and_path(&buffer);
-    println!("method {} path {}", pair.0, pair.1);
-    let get = b"GET / HTTP/1.1\r\n";
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
-    };
+    let (method, path) = parse_method_and_path(&buffer);
+    // By default use the 404 handler
+    let mut handler: ResponseHandler = error_404;
 
-    let root_path = std::path::Path::new(WEB_ROOT);
-    println!("{}", root_path.join(filename).display());
-    let contents = fs::read_to_string(root_path.join(filename)).unwrap();
-    let response = format!("{}{}", status_line, contents);
+    match &method[..] {
+        "GET" => {
+            // If the method is a get and the path exists as a file
+            if file_in_path(&path) {
+                // Use the file server handler
+                handler = file_server;
+            }
+        }
+        _ => println!("Unrecognized method {}", method),
+    }
+
+    let response = handler(&path);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
@@ -63,22 +68,68 @@ fn parse_method_and_path(buffer: &[u8]) -> (String, String) {
     let mut method = String::new();
     let mut string_accumulated = String::new();
     let mut element = 0;
+
+    // For each character
     for x in buffer {
         match *x as char {
+            // the only interesting thing is the first line
             '\r' => break,
+            // The method will be the first and then the URL
             ' ' => {
                 element = element + 1;
                 match element {
                     1 => method = string_accumulated.clone(),
-                    2 => path = string_accumulated.clone(),
+                    // When we have found the path everything is done
+                    2 => {
+                        path = string_accumulated.clone();
+                        break;
+                    }
                     _ => {}
                 }
-                println!("Acumulated {}", string_accumulated);
                 string_accumulated.clear();
             }
+            // Store the interesting char
             _ => string_accumulated.push(*x as char),
         };
     }
 
-    (method, path)
+    // Remove the leading '/' if there is a path, if not,
+    // The user is refering to index.html
+    let return_path: String = match path {
+        _ if path == "/" => String::from("index.html"),
+        _ => String::from(&path[1..]),
+    };
+
+    (method, return_path)
+}
+
+fn file_in_path(path_to_check: &str) -> bool {
+    let root_path = std::path::Path::new(WEB_ROOT);
+    let full_path_check = root_path.join(path_to_check);
+
+    // Check if file exists
+    match fs::metadata(full_path_check) {
+        // Return if it is a file
+        Ok(file_handler) => file_handler.is_file(),
+        // False if file does not exist or can't be opened
+        Err(_) => false,
+    }
+}
+
+fn file_server(file_path: &str) -> String {
+    // Serve the specified file content with an OK response
+    let status = "HTTP/1.1 200 OK\r\n\r\n";
+    let root_path = std::path::Path::new(WEB_ROOT);
+    let contents = fs::read_to_string(root_path.join(file_path)).unwrap();
+
+    format!("{}{}", status, contents)
+}
+
+fn error_404(_file_path: &str) -> String {
+    // Serve the 404.html to show an error to the user
+    let status = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+    let root_path = std::path::Path::new(WEB_ROOT);
+    let contents = fs::read_to_string(root_path.join("404.html")).unwrap();
+
+    format!("{}{}", status, contents)
 }
